6ab6f802de876b88eeb4e5ca7bf2bc2e51bfa413
Vladislav <mega.nozhkin@mail.ru>
2021-07-29 14:56:56 +0000

    the calculator is ready

--- lesson-6/calculator/form.php
+++ lesson-6/calculator/form.php
@@ -1,2 +1,17 @@
-<?php
-  
+<meta charset="utf-8">
+<form action="./engine/server.php" method="POST">
+  <input type="number" name="firstNumber" value="4">
+
+  <select name="operator">
+    <option>+</option>
+    <option>-</option>
+    <option>*</option>
+    <option>/</option>
+  </select>
+
+  <input type="number" name="secondNumber" value="7">
+
+  <input type="submit" name="send" value="=">
+  <span><?= $_GET['result'] ?></span>
+</form>
+
